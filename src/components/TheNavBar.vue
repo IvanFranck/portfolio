266ee5bb01970239136b88c9feceacb6fc66a5d9
nzima ivan <nzimaivan007@gmail.com>
2022-11-14 16:06:18 +0100

    💄 style navbar on small screens

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -8,21 +8,49 @@
         <h1 class="logo-text">Nzima Ivan</h1>
       </div>
 
-      <div role="navigation" class="font-roboto">
+      <div role="navigation" class="font-roboto navigation-lg-screen">
         <ul class="flex">
-          <li class="cursor-pointer">
+          <li>
             <a class="mx-4 cursor-pointer" href="/#about_section">About</a>
           </li>
-          <li class="cursor-pointer">
+          <li>
             <a class="mx-4 cursor-pointer" href="/#experience_section">Experience</a>
           </li>
-          <li class="cursor-pointer">
+          <li>
             <a class="mx-4 cursor-pointer" href="/#work_section">Works</a>
           </li>
-          <li class="cursor-pointer">
+          <li>
             <a class="mx-4 cursor-pointer">Contact</a>
           </li>
         </ul>
+      </div>
+
+      <!-- Mobile navigation -->
+      <div :class="{ 'navigation-sm-screen': true, 'aside-menu__active': hamCliked }">
+        <button
+          @click="handleHamClick"
+          :class="{ 'ham relative': !hamCliked, ham__active: hamCliked }"
+        >
+          <div class="ham-box">
+            <div class="ham-box-inner"></div>
+          </div>
+        </button>
+        <aside class="aside-menu flex flex-col justify-center items-center">
+          <ol class="w-full flex flex-col items-center py-20">
+            <li>
+              <a class="mx-4 cursor-pointer" href="/#about_section">About</a>
+            </li>
+            <li>
+              <a class="mx-4 cursor-pointer" href="/#experience_section">Experience</a>
+            </li>
+            <li>
+              <a class="mx-4 cursor-pointer" href="/#work_section">Works</a>
+            </li>
+            <li>
+              <a class="mx-4 cursor-pointer">Contact</a>
+            </li>
+          </ol>
+        </aside>
       </div>
     </div>
   </header>
@@ -35,10 +63,17 @@ import useScrollDirection from "../composables/useScrollDirection";
 const scrollDirection = ref(useScrollDirection("down"));
 const style = ref();
 const scrolledToTop = ref<boolean>(true);
-// const scrollStyle = reactive({ style });
+const hamCliked = ref<boolean>(false);
 
 const handleScroll = () => {
   scrolledToTop.value = window.pageYOffset < 50;
+};
+
+const handleHamClick = () => {
+  hamCliked.value = !hamCliked.value;
+  if (document.body.classList.contains("blur-bg"))
+    document.body.classList.remove("blur-bg");
+  else document.body.classList.add("blur-bg");
 };
 
 watchEffect(() => {
@@ -71,6 +106,70 @@ watch([scrollDirection, scrolledToTop], () => {
 <style scoped lang="scss">
 @import "/src/assets/scss/_mixin.scss";
 
+.navigation-sm-screen {
+  display: none;
+
+  .aside-menu {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    position: fixed;
+    background-color: var(--bg);
+    height: 100vh;
+    transform: translateX(100vw);
+    visibility: hidden;
+    transition: var(--transition-default);
+  }
+}
+.aside-menu__active {
+  .aside-menu {
+    position: fixed;
+    top: 0px;
+    bottom: 0px;
+    right: 0px;
+    z-index: 9;
+    background-color: var(--bg);
+    height: 100vh;
+    width: min(75vw, 400px);
+    transform: translateX(0vw);
+    visibility: visible;
+    transition: var(--transition-default);
+
+    ol {
+      li {
+        text-align: center;
+        width: 100%;
+        margin-bottom: 32px;
+        font-size: 18px;
+        font-weight: 500;
+
+        a {
+          display: inline-block;
+          width: 100%;
+          padding: 3px 20px 20px;
+        }
+      }
+    }
+  }
+}
+
+.aside-menu {
+  &--box {
+    display: flex;
+    -webkit-box-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    align-items: center;
+    padding: 50px 10px;
+    position: fixed;
+    width: min(75vw, 400px);
+    height: 100vh;
+    outline: 0px;
+    background-color: var(--bg);
+    box-shadow: -10px 0px 30px -15px var(--blue-shadow);
+  }
+}
+
 @mixin scrolled-nav-link {
   &:hover,
   &:active,
@@ -82,9 +181,8 @@ watch([scrollDirection, scrolledToTop], () => {
 
 #scrollUp {
   transform: translateY(0px);
-  background-color: var(--color-blue);
+  background-color: var(--bg);
   box-shadow: 0 10px 30px -10px var(--blue-shadow);
-  color: var(--bg);
 
   ul {
     li {
@@ -94,7 +192,7 @@ watch([scrollDirection, scrolledToTop], () => {
 }
 
 #scrollDown {
-  transform: translateY(-100px);
+  transform: translateY(-120px);
   box-shadow: 0 20px 30px -10px var(--blue-shadow);
   ul {
     li {
@@ -103,7 +201,8 @@ watch([scrollDirection, scrolledToTop], () => {
   }
 }
 
-ul {
+ul,
+ol {
   li {
     @include link;
     font-size: var(--fz-md);
@@ -122,8 +221,22 @@ header > div {
     padding: var(--padding-xs);
   }
 
+  #scrollDown {
+    transform: translateY(-75px);
+  }
+
   .nav {
     display: none;
+  }
+}
+
+@media (max-width: 768px) {
+  .navigation-lg-screen {
+    display: none;
+  }
+
+  .navigation-sm-screen {
+    display: block;
   }
 }
 </style>
